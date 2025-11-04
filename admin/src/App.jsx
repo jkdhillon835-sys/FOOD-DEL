1863e6e582c4caf293958ddcff1eb26af4f8498e
Jasleen Kaur <jkdhillon835@gmail.com>
2025-11-04 22:02:38 +0530

    fix: properly terminated string literal in admin/App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Add from './pages/Add/Add'
 import List from './pages/List/List'
 import Orders from './pages/Orders/Orders'
@@ -9,25 +9,25 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-
- const url = "http://localhost:4000"
+  // Make sure the string is closed and no extra spaces inside the quotes!
+  const url = "https://food-del-backend-5wdb.onrender.com";
 
   return (
     <div>
       <ToastContainer/>
       <Navbar/>
-     <hr/>
-     <div className="app-content">
-      <Sidebar/>
-      <Routes>
-       <Route path="/add" element={<Add url = {url}/>}/>
-       <Route path="/list" element={<List url = {url} />} />
-       <Route path="/orders" element={<Orders url = {url} />} />
-
-      </Routes>
-     </div>
+      <hr/>
+      <div className="app-content">
+        <Sidebar/>
+        <Routes>
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
+          <Route path="/orders" element={<Orders url={url} />} />
+        </Routes>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
+
